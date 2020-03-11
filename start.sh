f98b40003e4745dc90afa58659ade0172b92118b
Andrew Druzhinin <andrey@drzhn.in>
2020-03-11 14:30:01 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ docker run \
     --name ikev2-vpn-server \
     -p 500:500/udp \
     -p 4500:4500/udp \
+    -p 80:80 \
     -v /lib/modules:/lib/modules:ro \
     -v "$PWD/data/certs/certs:/usr/local/etc/ipsec.d/certs" \
     -v "$PWD/data/certs/private:/usr/local/etc/ipsec.d/private" \
